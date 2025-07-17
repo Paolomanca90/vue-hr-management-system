11ad053137e02a42bdc61f1589c27d9593cd647b
Paolo Leucio Manca <132942470+Paolomanca90@users.noreply.github.com>
2025-07-17 16:39:19 +0100

    fix pacchettizzazione

--- build-client.js
+++ build-client.js
@@ -2,12 +2,12 @@ import { execSync } from 'child_process'
 
 const client = process.argv[2]
 if (!client) {
-  console.error('Specifica il cliente: npm run build:client client-a')
+  console.error('Specifica il cliente: pnpm run build:client client-a')
   process.exit(1)
 }
 
 // Build del core se necessario
 execSync('pnpm run build:core', { stdio: 'inherit' })
 
-// Build del cliente specifico
-execSync(`pnpm --filter @myapp/${client}-features build`, { stdio: 'inherit' })
+// Build del cliente specifico 
+execSync(`pnpm --filter @vue-hr-management-system/${client}-features build`, { stdio: 'inherit' })
