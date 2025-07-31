f1783d36de8e1cde1811c7f48efa501ee303a3ee
Paolo Leucio Manca <132942470+Paolomanca90@users.noreply.github.com>
2025-07-31 09:41:34 +0100

    nuovi metodi

--- packages/main-app/src/services/accessiService.ts
+++ packages/main-app/src/services/accessiService.ts
@@ -1,6 +1,6 @@
 import { getApiConfig } from '@/config/api'
 
-export interface Accessi {
+export interface Accesso {
     codice: string,
     descrizione: string,
     formula: string
@@ -9,7 +9,7 @@ export interface Accessi {
 class AccessiService {
   private config = getApiConfig()
 
-  async getTabAccessi(): Promise<Accessi[]> {
+  async getTabAccessi(): Promise<Accesso[]> {
     try {
       const response = await fetch(`${this.config.baseUrl}${this.config.endpoints.accessi}`, {
         method: 'GET',
@@ -17,6 +17,54 @@ class AccessiService {
           ...this.config.defaultHeaders,
           'Authorization': 'Bearer ' + localStorage.getItem('auth_token'),
         }
+      })
+
+      if (!response.ok) {
+        // Se la risposta non è ok, prova a leggere il messaggio di errore
+        const errorText = await response.text()
+        throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`)
+      }
+
+      return await response.json()
+
+    } catch (error) {
+      throw new Error('Errore di connessione al server: ' + error)
+    }
+  }
+
+  async addAccesso(accesso:Accesso): Promise<Accesso> {
+    try {
+      const response = await fetch(`${this.config.baseUrl}${this.config.endpoints.accessi}`, {
+        method: 'POST',
+        headers: {
+          ...this.config.defaultHeaders,
+          'Authorization': 'Bearer ' + localStorage.getItem('auth_token'),
+        },
+        body: JSON.stringify(accesso)
+      })
+
+      if (!response.ok) {
+        // Se la risposta non è ok, prova a leggere il messaggio di errore
+        const errorText = await response.text()
+        throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`)
+      }
+
+      return await response.json()
+
+    } catch (error) {
+      throw new Error('Errore di connessione al server: ' + error)
+    }
+  }
+
+  async editAccesso(accesso:Accesso): Promise<Accesso> {
+    try {
+      const response = await fetch(`${this.config.baseUrl}${this.config.endpoints.accessi}`, {
+        method: 'PUT',
+        headers: {
+          ...this.config.defaultHeaders,
+          'Authorization': 'Bearer ' + localStorage.getItem('auth_token'),
+        },
+        body: JSON.stringify(accesso)
       })
 
       if (!response.ok) {
--- packages/main-app/src/services/userService.ts
+++ packages/main-app/src/services/userService.ts
@@ -49,6 +49,77 @@ class UserService {
     }
   }
 
+  async newUser(user:User): Promise<User> {
+    try {
+      const response = await fetch(`${this.config.baseUrl}${this.config.endpoints.users}`, {
+        method: 'POST',
+        headers: {
+          ...this.config.defaultHeaders,
+          'Authorization': 'Bearer ' + localStorage.getItem('auth_token'),
+        },
+        body: JSON.stringify(user)
+      })
+
+      if (!response.ok) {
+        // Se la risposta non è ok, prova a leggere il messaggio di errore
+        const errorText = await response.text()
+        throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`)
+      }
+
+      return await response.json()
+
+    } catch (error) {
+      throw new Error('Errore di connessione al server: ' + error)
+    }
+  }
+
+  async editUser(user:User): Promise<User> {
+    try {
+      const response = await fetch(`${this.config.baseUrl}${this.config.endpoints.users}`, {
+        method: 'PUT',
+        headers: {
+          ...this.config.defaultHeaders,
+          'Authorization': 'Bearer ' + localStorage.getItem('auth_token'),
+        },
+        body: JSON.stringify(user)
+      })
+
+      if (!response.ok) {
+        // Se la risposta non è ok, prova a leggere il messaggio di errore
+        const errorText = await response.text()
+        throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`)
+      }
+
+      return await response.json()
+
+    } catch (error) {
+      throw new Error('Errore di connessione al server: ' + error)
+    }
+  }
+
+  // async deleteUser(user:User): Promise<boolean> {
+  //   try {
+  //     const response = await fetch(`${this.config.baseUrl}${this.config.endpoints.deleteUser}`, {
+  //       method: 'DELETE',
+  //       headers: {
+  //         ...this.config.defaultHeaders,
+  //         'Authorization': 'Bearer ' + localStorage.getItem('auth_token'),
+  //       }
+  //     })
+
+  //     if (!response.ok) {
+  //       // Se la risposta non è ok, prova a leggere il messaggio di errore
+  //       const errorText = await response.text()
+  //       throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`)
+  //     }
+
+  //     return await response.json()
+
+  //   } catch (error) {
+  //     throw new Error('Errore di connessione al server: ' + error)
+  //   }
+  // }
+
   async getGruppiUtente(): Promise<GruppiUtenti[]> {
     try {
       const response = await fetch(`${this.config.baseUrl}${this.config.endpoints.gruppiUtente}`, {
